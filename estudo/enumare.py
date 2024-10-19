61af0bb05523463bbf8aa6efae5768e8cd4b452b
Robson Sandro <robsonsandro@hotmail.com>
2024-10-19 15:15:26 -0300

    enumarete

---
+++ estudo/enumare.py
@@ -0,0 +1,15 @@
+lista= ["mairia", "helena", "joao"]
+lista.append("helen")
+
+listaEnumarada=list(enumerate(lista))
+
+
+
+print(listaEnumarada)
+
+ ##O enumarete, enumera minha lista nao sendo exclusiva comecar com 0
+
+
+
+    
+    
