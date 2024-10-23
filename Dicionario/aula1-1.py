04ac2333b83ab2c1c128bb798465c1e7d00f38ba
Robson Sandro <robsonsandro@hotmail.com>
2024-10-23 20:27:09 -0300

    atualizacao

--- Dicionario/aula1-1.py
+++ Dicionario/aula1-1.py
@@ -1,18 +1,21 @@
 agenda={}
 while True:
     nome=input("Digite seu nome:")
-    tel=input("infome seu telefone: ")
-    try:
+    while True:
+     tel=input("infome seu telefone: ")
+     try:
         tel_int=int(tel)    
         agenda [nome]=tel_int
-    except:
+        break
+     except:
         print("digite um numero valido.")
     cpf=input("digite seu cpf: ")
-    agenda [nome]=cpf
+    agenda ["cpf"]=cpf
     resp=input("voce deseja continuar add contatos: (s/n)")
     if(resp.lower()=='n'):
         print("saindo do programa")
         break
 print(agenda)
+
 
 
